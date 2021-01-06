967ba4964ed6b109bf2ea8889972f3097213ae63
juliamj <64139476+juliamj@users.noreply.github.com>
2021-01-06 19:56:13 +0100

    first draft und erste routes definiert

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item active><NuxtLink to="/"> Home </NuxtLink></b-nav-item>
+      <b-nav-item><NuxtLink to="/profil"> Profil </NuxtLink></b-nav-item>
+      <b-nav-item><NuxtLink to="/einstellungen"> Einstellungen </NuxtLink></b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+</style>
